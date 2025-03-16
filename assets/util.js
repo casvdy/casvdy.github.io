a9e5d8226149404bbacfe802845628f930980827
aswin <aswinxn@protonmail.com>
2025-03-16 14:45:37 +0530

    fix contact year missing

--- assets/util.js
+++ assets/util.js
@@ -1,78 +1,98 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('.year').innerHTML = (new Date().getFullYear());
-    const dropdownParents = document.querySelectorAll('.dropdown-parent');
 
-    function closeAllDropdowns() {
-        dropdownParents.forEach(parent => {
-            parent.classList.remove('active');
-        });
+    // Use try-catch blocks for diff utils so if one fails, the rest of it won't.
+
+    // ADD THE YEAR AT FOOTER
+    try {
+        document.querySelector('.year').innerHTML = (new Date().getFullYear());                
+    } catch (error) {
+        console.log("cannot update year: ", error)
     }
 
-    dropdownParents.forEach(parent => {
-        parent.addEventListener('click', function (e) {
-            if (window.innerWidth <= 768) {
-                // Check if the clicked element is a link inside the dropdown
-                if (!e.target.closest('.dropdown a')) {
-                    e.preventDefault(); // Only prevent default if not clicking a dropdown link
-                    this.classList.toggle('active');
+    // CONFIGURE DROPDOWN CLICKS
+    try {
+        const dropdownParents = document.querySelectorAll('.dropdown-parent');
+        function closeAllDropdowns() {
+            dropdownParents.forEach(parent => {
+                parent.classList.remove('active');
+            });
+        }
+    
+        dropdownParents.forEach(parent => {
+            parent.addEventListener('click', function (e) {
+                if (window.innerWidth <= 768) {
+                    // Check if the clicked element is a link inside the dropdown
+                    if (!e.target.closest('.dropdown a')) {
+                        e.preventDefault(); // Only prevent default if not clicking a dropdown link
+                        this.classList.toggle('active');
+                    }
                 }
+            });
+        });
+    
+        document.addEventListener('click', function (e) {
+            if (!e.target.closest('.dropdown-parent')) {
+                closeAllDropdowns();
             }
         });
-    });
+    
+        window.addEventListener('resize', closeAllDropdowns);        
+    } catch (error) {
+        console.log('[DROPDOWN ERROR] ', error)
+    }
 
-    document.addEventListener('click', function (e) {
-        if (!e.target.closest('.dropdown-parent')) {
-            closeAllDropdowns();
-        }
-    });
+    // FOR GALLERY ITEMS IN GALLERY PAGE (FOR NOW)
+    try {
 
-    window.addEventListener('resize', closeAllDropdowns);
+        let items = document.querySelectorAll('.gallery-item');
 
-
-    let items = document.querySelectorAll('.gallery-item');
-
-    items.forEach((item) => {
-        item.addEventListener('click', (e) => {
-            // Create modal elements
-            const modal = document.createElement('div');
-            modal.className = 'gallery-modal active';
-            document.body.classList.add('modal-open');
-
-            const content = document.createElement('div');
-            content.className = 'gallery-modal-content';
-
-            const img = new Image();
-            const src = document.createElement('p', {class: "author"});
-            src.textContent = item.querySelector('img').getAttribute('data-src');
-            img.className = 'gallery-modal-img';
-            img.src = item.querySelector('img').src;
-            img.alt = item.querySelector('img').alt;
-
-
-            // Build modal structure
-            content.appendChild(src);
-            content.appendChild(img);
-            modal.appendChild(content);
-            document.body.appendChild(modal);
-
-            // Close modal on click
-            modal.addEventListener('click', (e) => {
-                if (modal === e.target) {
-                    document.body.classList.remove('modal-open');
-                    modal.remove();
-                }
+        items.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                // Create modal elements
+                const modal = document.createElement('div');
+                modal.className = 'gallery-modal active';
+                document.body.classList.add('modal-open');
+    
+                const content = document.createElement('div');
+                content.className = 'gallery-modal-content';
+    
+                const img = new Image();
+                const src = document.createElement('p', {class: "author"});
+                src.textContent = item.querySelector('img').getAttribute('data-src');
+                img.className = 'gallery-modal-img';
+                img.src = item.querySelector('img').src;
+                img.alt = item.querySelector('img').alt;
+    
+    
+                // Build modal structure
+                content.appendChild(src);
+                content.appendChild(img);
+                modal.appendChild(content);
+                document.body.appendChild(modal);
+    
+                // Close modal on click
+                modal.addEventListener('click', (e) => {
+                    if (modal === e.target) {
+                        document.body.classList.remove('modal-open');
+                        modal.remove();
+                    }
+                });
+    
+                // Close on ESC key
+                document.addEventListener('keydown', function escClose(e) {
+                    if (e.key === 'Escape') {
+                        document.body.classList.remove('modal-open');
+                        modal.remove();
+                        document.removeEventListener('keydown', escClose);
+                    }
+                });
             });
+        });        
+    } catch (error) {
+        console.log('[GALLERY-ITEM] ',error)
+    }
 
-            // Close on ESC key
-            document.addEventListener('keydown', function escClose(e) {
-                if (e.key === 'Escape') {
-                    document.body.classList.remove('modal-open');
-                    modal.remove();
-                    document.removeEventListener('keydown', escClose);
-                }
-            });
-        });
-    });
+
 });
 
 function toggleMenu() {
